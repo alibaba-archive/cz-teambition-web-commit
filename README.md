bccfc790cd0bf4e33b2d142df32ae6b710a7bb29
Boshen Chen <boshenc@gmail.com>
2016-08-17 11:17:21 +0800

    根据任务ID调整模板
    * 删除了 scope
    * 删除了 FEATURE, CHORE 这部分的前缀
    * 添加了 任务ID 前缀
    * 添加了 hotfix 标示

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](https://www.npmjs.com/package/commitizen)
 
-Teambition 前端 Git Commit 规范
-------------------------------
+# 如何向 Teambition Web 贡献你的代码
 
-使用 [commitizen](https://www.npmjs.com/package/commitizen)
+- Teambition git-flow 规范： [standard](https://github.com/teambition/standard/blob/master/git-flow.md)
 
-采用 Teambition git-flow 规范： [standard](https://github.com/teambition/standard/blob/master/git-flow.md)
+每一次的改动都应该尽量与 Teambition 中的任务对应，方便回溯整个改动的起因和结果，
+所以为了保证 Commit Tree 的规范以及减少改动的歧义，我们现在做出以下的规范：
+
+- 使用 Teambition 中的任务 ID 就可以作为每次 Commit 的前缀。
+
+    > 如果你还没有进入对应的项目，请联系对应的负责人将你加入对应的任务组
+
+    > 如果你所在的项目还没有开启任务 ID，请在插件中将 `任务 ID` 开启
+
+- 使用 [Commitizen](https://github.com/commitizen/cz-cli) 和 [Teambition 前端 Git Commit 规范](https://github.com/teambition/cz-teambition-web-commit) 提供方便的 Commit 模版
+
+- 请在每次提交改动的时候，检查这次改动对应的 Commit Tree 是否符合规则，有疑问的 Commit Tree 需要无条件的进行 Rebase & Edit
+
+#### 使用说明
+
+1. 安装 [Commitizen](https://github.com/commitizen/cz-cli), `npm i commitizen -g` (确保项目中的 `cz-teambition-web-commit` 也已经安装)
+
+2. `git commit` 的时候使用 `git cz` 或者 `npm run commit`
+
+3. 提交你的改动
+
+#### 示例
+```
+TB-3248: 修改 tb commitizen 模板
+
+现在开始需要添加任务ID了
+```
--- index.js
+++ index.js
@@ -5,46 +5,28 @@ module.exports = {
   prompter: function(cz, commit) {
     cz.prompt([
       {
-        type: 'list',
-        name: 'type',
-        message: 'Commit 类型：',
-        choices: [
-        {
-          name: 'FEATURE:  功能开发',
-          value: 'FEATURE'
-        }, {
-          name: 'ISSUE:    普通 BUG 修复',
-          value: 'ISSUE'
-        }, {
-          name: 'HOTFIX:   线上 BUG 修复',
-          value: 'HOTFIX'
-        }, {
-          name: 'RELEASE:  发布相关的改动',
-          value: 'RELEASE'
-        }, {
-          name: 'REFACTOR: 重构',
-          value: 'REFACTOR'
-        }, {
-          name: 'CHORE:    其它更改， 比如更新工具或者更改文档',
-          value: 'CHORE'
-        }]
-      }, {
         type: 'input',
-        name: 'scope',
-        message: '更改范围， 比如 views/board, models/task, lib/dropdown, webpack\n'
-      }, {
+        name: 'id',
+        message: '任务ID'
+      },
+      {
+			  type: 'confirm',
+        name: 'hotfix',
+        default: false,
+        message: 'hotfix?'
+      },
+      {
         type: 'input',
         name: 'subject',
-        message: 'Commit 描述: 修复.., 添加.., 删除.. \n'
+        message: 'Commit 描述: 修复XX功能..., 添加XX模块... \n'
       }, {
         type: 'input',
         name: 'body',
-        message: '还有其它内容吗？ 比如更改理由, 更改细节, 或者什么奇怪的问题\n'
+        message: '还有其它更改理由, 更改细节, 或者需要注意的地方吗\n'
       }
     ]).then(function(answers) {
 
       var maxLineWidth = 80;
-
       var wrapOptions = {
         trim: true,
         newline: '\n',
@@ -52,13 +34,20 @@ module.exports = {
         width: maxLineWidth
       };
 
-      var scope = answers.scope.trim();
-      scope = scope ? '(' + answers.scope.trim() + ')' : '';
-
-      var message = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
-
+      var id = answers.id.trim();
+      var isHotfix = answers.hotfix;
+      var subject = answers.subject.trim();
       var body = answers.body.trim();
-      message = body ? message + '\n\n' + wrap(body, wrapOptions) : message;
+
+      var message = [
+        id ? id + ':' : '',
+        isHotfix ? '(hotfix)' : '',
+        subject
+      ].filter(function(s){ return s }).join(' ')
+
+      if (body) {
+        message = message + '\n\n' + wrap(body, wrapOptions)
+      }
 
       commit(message);
     });
